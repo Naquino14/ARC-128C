c5510cf3f9253c1ffbc4436ab0f98968c55e1f63
Nathaniel Aquino <naquino14@outlook.com>
2025-01-18 13:29:47 -0500

    Create project

---
+++ include/ARC-128C.h
@@ -0,0 +1,6 @@
+#ifndef ARC128C_H
+#define ARC128C_H
+
+void foo();
+
+#endif // ARC128C_H
---
+++ test/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <ARC-128C.h>
+
+int main(void) {
+    printf("Bazinga\n");
+    foo();
+}
